8930e42b22ff4a4c84e178b09c3f4e46d9c055bf
Adam Brewer <adamhb321@gmail.com>
2022-10-31 15:20:40 -0400

    Git pulling functionality

--- src/backman/backman.js
+++ src/backman/backman.js
@@ -4,25 +4,36 @@ const spawn = require("child_process").spawn;
 const express = require("express");
 const cors = require("cors");
 const https = require("https");
+const { exit } = require("process");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
 require("dotenv").config({ path: "../../.env" });
-const port = process.env.REACT_APP_BACKMAN_PORT ? process.env.REACT_APP_BACKMAN_PORT : 6969;
+const _port = process.env.REACT_APP_BACKMAN_PORT || 6969;
+const _hostname = process.env.REACT_APP_HOSTNAME || "0.0.0.0";
 
-app.use(cors({origin: true}));
-app.use(function(req, res, next) {
+app.use(cors({ origin: true }));
+app.use(function (req, res, next) {
     req.rawBody = "";
     req.setEncoding("utf8");
-    req.on("data", function(chunk) {
+    req.on("data", function (chunk) {
         req.rawBody += chunk;
     });
-    req.on("end", function() {
+    req.on("end", function () {
         next();
     });
 });
 
+function pull_git_repo_and_rebuild() {
+    const spawned_proc = spawn(`git stash save; git fetch origin master && git reset --hard FETCH_HEAD && git clean -df && npm run build-prod && service apache2 restart; git stash apply`,
+    { cwd: __filename.substring(0, __filename.lastIndexOf('/', __filename.lastIndexOf('/') - 1)), shell: true });
+    spawned_proc.stdout.on('data', (data) => console.log(data.toString()));
+    spawned_proc.stderr.on('data', (data) => console.log(data.toString()));
+    spawned_proc.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
 function _check_err(err) {
-    if(err){
+    if (err) {
         console.error(err.message);
     }
 }
@@ -41,11 +52,12 @@ function get_blog_posts() {
 }
 
 function verify_webhook_signature(req, res, next) {
-    if (!req.rawBody){
+    const secret = process.env.PUSHUP_SECRET;
+    if (!secret) return next("PUSHUP_SECRET not provided, webhook verification disabled")
+    if (!req.rawBody) {
         return next("Request body empty");
     }
     console.log("Received pushup");
-    const secret = process.env.PUSHUP_SECRET;
     const sigHeaderName = "X-Hub-Signature-256";
     const sigHashAlg = "sha256";
     const sig = Buffer.from(req.get(sigHeaderName) || "", "utf8");
@@ -58,31 +70,29 @@ function verify_webhook_signature(req, res, next) {
 }
 
 app.get("/api/get-blog-posts", (req, res) => {
-    get_blog_posts().then((posts)=>res.json({posts: posts}));
+    get_blog_posts().then((posts) => res.json({ posts: posts }));
 });
 
-if(process.env.PUSHUP_SECRET) {
-    app.post("/api/pushup", verify_webhook_signature, () => {
+if (process.env.PUSHUP_SECRET || process.env.NODE_ENV != "production") {
+    console.log("register pushup")
+    app.post("/api/pushup", verify_webhook_signature, (req, res, next) => {
         console.log("Git push received, update when possible!");
-    });
-    app.use((err, req, res) => {
-        if (err) console.error(err);
-        res.status(403).send("Request body was not signed or verification failed");
+        res.status(200).send("Triggering git pull and app restart")
     });
 }
-
-if(process.env.NODE_ENV == "production") {
+pull_git_repo_and_rebuild()
+if (process.env.NODE_ENV == "production") {
     https.createServer(
         {
             key: fs.readFileSync("/etc/letsencrypt/live/adabrew.com/privkey.pem"),
             cert: fs.readFileSync("/etc/letsencrypt/live/adabrew.com/cert.pem"),
             ca: fs.readFileSync("/etc/letsencrypt/live/adabrew.com/chain.pem")
         }, app).listen(port, () => {
-        console.log(`(HTTPS) Backman on port ${port}`);
-    });
+            console.log(`(HTTPS) Backman on port ${port}`);
+        });
 }
 else {
-    app.listen(port, () => {
-        console.log(`(HTTP) Backman on port ${port}`);
+    app.listen(_port, _hostname, () => {
+        console.log(`(HTTP) Backman host ${_hostname} on port ${_port}`);
     });
 }
